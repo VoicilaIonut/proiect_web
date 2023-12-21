d54d708d8aa06b7fdfb41e20db294acb117fbb32
VoicilaIonut <mariusvoicila03@yahoo.com>
2023-12-21 23:12:14 +0300

    roles

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Proiect.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Proiect.Controllers
+{
+    [Route("api/roles")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<User> _userManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        [HttpGet("{roleId}")]
+        public async Task<ActionResult<IdentityRole>> GetRoleById(string roleId)
+        {
+            var role = await _roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+            {
+                return NotFound("Role not found");
+            }
+
+            return Ok(role);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllRoles()
+        {
+            var roles = _roleManager.Roles;
+            return Ok(roles);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRole([FromBody] IdentityRole role)
+        {
+            var result = await _roleManager.CreateAsync(role);
+
+            if (result.Succeeded)
+            {
+                return CreatedAtAction(nameof(GetRoleById), new { roleId = role.Id }, role);
+            }
+
+            return BadRequest(result.Errors);
+        }
+
+        [HttpPut("{roleId}")]
+        public async Task<IActionResult> UpdateRole(string roleId, [FromBody] IdentityRole role)
+        {
+            var existingRole = await _roleManager.FindByIdAsync(roleId);
+            if (existingRole == null)
+            {
+                return NotFound("Role not found");
+            }
+
+            existingRole.Name = role.Name;
+            var result = await _roleManager.UpdateAsync(existingRole);
+
+            if (result.Succeeded)
+            {
+                return NoContent();
+            }
+
+            return BadRequest(result.Errors);
+        }
+
+        [HttpDelete("{roleId}")]
+        public async Task<IActionResult> DeleteRole(string roleId)
+        {
+            var role = await _roleManager.FindByIdAsync(roleId);
+            if (role == null)
+            {
+                return NotFound("Role not found");
+            }
+
+            var result = await _roleManager.DeleteAsync(role);
+
+            if (result.Succeeded)
+            {
+                return NoContent();
+            }
+
+            return BadRequest(result.Errors);
+        }
+
+        [HttpPost("assignrole")]
+        public async Task<IActionResult> AssignRole(string userId, string roleId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            var role = await _roleManager.FindByIdAsync(roleId);
+
+            if (user == null || role == null)
+            {
+                return NotFound("User or role not found");
+            }
+
+            var result = await _userManager.AddToRoleAsync(user, role.Name);
+
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+
+            return BadRequest(result.Errors);
+        }
+
+        [HttpPost("removerole")]
+        public async Task<IActionResult> RemoveRoleFromUser(Guid userId, string roleId)
+        {
+            var user = await _userManager.FindByIdAsync(userId.ToString());
+            var role = await _roleManager.FindByIdAsync(roleId);
+
+            if (user == null || role == null)
+            {
+                return NotFound("User or role not found");
+            }
+
+            var result = await _userManager.RemoveFromRoleAsync(user, role.Name);
+
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+
+            return BadRequest(result.Errors);
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,76 @@
+﻿using Proiect.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+using Proiect.Models.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Proiect.Services.Interfaces;
+
+
+namespace Proiect.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("{userId}")]
+        public ActionResult<User> GetUserById(Guid userId)
+        {
+            var user = _userService.GetUserById(userId.ToString());
+            Console.WriteLine(user.UserName);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetAllUsers()
+        {
+            var users = _userService.GetAllUsers();
+            foreach(var user in users)
+            {
+                Console.WriteLine(user.UserName);
+            }
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] CreateUserDto userDto)
+        {
+            var user = _userService.CreateUser(userDto);
+            return CreatedAtAction(nameof(GetUserById), new { userId = user.Id }, user);
+        }
+
+
+        [HttpDelete("{userId}")]
+        public IActionResult DeleteUser(Guid userId)
+        {
+            var existingUser = _userService.GetUserById(userId.ToString());
+            if (existingUser == null)
+            {
+                return NotFound();
+            }
+
+            _userService.DeleteUser(userId.ToString());
+
+            return NoContent();
+        }
+    }
+}
